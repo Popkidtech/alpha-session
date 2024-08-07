c63fcdd610dfb7b987c6e6148d460c18d521d87b
Popkidtech <popkidgeee@gmail.com>
2024-08-07 08:06:11 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -56,21 +56,21 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Venocyber_Tech.sendMessage(Pair_Code_By_Venocyber_Tech.user.id, { text: '' + b64data });
 
                let VENOCYBER_MD_TEXT = `
-*_Pair Code Connected by Venocyber Tech_*
+*_Pair Code Connected by Popkid Tech_*
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU CHOOSEN VENOCYBER-MD 』*
+║ *『 WOW YOU CHOOSEN POPKID-MD 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚══════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@JASTINMTEWA-vn9pl_
-║❒ *Owner:* _https://wa.me/message/A4QG2JZKBXFTN1_
-║❒ *Repo:* _https://github.com/Kingjux/venocyber-md_
-║❒ *WaGroup:* _https://chat.whatsapp.com/HSln3blDuuuKvC8njxyCCN_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaYauR9ISTkHTj4xvi1l_
-║❒ *Plugins:* _https://github.com/Kingjux/venocyber-md-plugins_
+║❒ *Ytube:* _youtube.com/@POPKID254_
+║❒ *Owner:* _https://wa.me/message/254111385747_
+║❒ *Repo:* _https://github.com/Popkidtech/POPKID-MD_
+║❒ *WaGroup:* _https://chat.whatsapp.com/IwT9XmuZKhy61cLbsM1M6H_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VadQrNI8KMqo79BiHr3l_
+║❒ *Plugins:* _https://github.com/Popkidtech/POPKID-MD_
 ╚══════════════════════╝ 
 _____________________________________
 
